f52e0a8f6a617e7261147b769a1c6523ff88f464
Shikhar Agarwal <Shikharag.sa2002@gmail.com>
2024-09-13 18:51:03 +0530

    feat: validating meta verification requests

--- routes/listenToMessages.dart
+++ routes/listenToMessages.dart
@@ -7,10 +7,14 @@ Response onRequest(RequestContext context) {
   final request = context.request;
 
   final params = request.uri.queryParameters;
-
+  final kVerificationToken = '1234567890';
   final hubMode = params['hub_mode'] ?? 'NA';
   final hubVerifyToken = params['hub_verify_token'] ?? 'NA';
   final hubChallenge = params['hub_challenge'] ?? 'NA';
 
-  return Response(body: 'HubMode: $hubMode\nHubVerifyToken: $hubVerifyToken\nHubChallenge: $hubChallenge');
+  if (hubVerifyToken== kVerificationToken) {
+    return Response(body: hubChallenge);
+  } else {
+    return Response(statusCode: 400, body: 'Invalid Verification Token');
+  }
 }
