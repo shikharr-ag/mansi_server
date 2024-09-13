403d84e3049a373cfb175795ada654263a909774
Shikhar Agarwal <Shikharag.sa2002@gmail.com>
2024-09-13 19:15:39 +0530

    updated validation;

--- routes/listenToMessages.dart
+++ routes/listenToMessages.dart
@@ -8,11 +8,11 @@ Response onRequest(RequestContext context) {
 
   final params = request.uri.queryParameters;
   final kVerificationToken = '1234567890';
-  final hubMode = params['hub_mode'] ?? 'NA';
-  final hubVerifyToken = params['hub_verify_token'] ?? 'NA';
-  final hubChallenge = params['hub_challenge'] ?? 'NA';
-
-  if (hubVerifyToken== kVerificationToken) {
+  final hubMode = params['hub.mode'] ?? 'NA';
+  final hubVerifyToken = params['hub.verify_token'] ?? 'NA';
+  final hubChallenge = params['hub.challenge'] ?? 'NA';
+  // return Response(body: 'Params are $params');
+  if (hubVerifyToken == kVerificationToken) {
     return Response(body: hubChallenge);
   } else {
     return Response(statusCode: 400, body: 'Invalid Verification Token');
