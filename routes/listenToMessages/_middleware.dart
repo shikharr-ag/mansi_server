e7c2b5c6facb77e2076aa37cfda104263ad1c657
Shikhar Agarwal <Shikharag.sa2002@gmail.com>
2024-09-16 20:33:30 +0530

    testing send msg with middleware

--- routes/listenToMessages/_middleware.dart
+++ routes/listenToMessages/_middleware.dart
@@ -61,18 +61,18 @@ Handler middleware(Handler handler) {
 Future<bool> replyToUser(String name, String messageBody,
     {required RequestLogger logger}) {
   try {
-    return Future.delayed(
-      Duration(seconds: 3),
-    ).then((_) {
-      logger.info('\nMESSAGE SENT!\n');
-      return true;
-    });
-    // logger.debug('\n\nMessage sent to Mansi for processing\n\n');
+    // return Future.delayed(
+    //   Duration(seconds: 3),
+    // ).then((_) {
+    //   logger.info('\nMESSAGE SENT!\n');
+    //   return true;
+    // });
+    logger.debug('\n\nMessage sent to Mansi for processing\n\n');
     // Future.delayed(Duration(seconds: 5)).then((_) async{
     //   logger.debug('\n\nMessage Processed in BG\n\n');
     // });
-    // final messageSendEndpoint =
-    //     Uri.parse('https://graph.facebook.com/v20.0/390332304171825/messages');
+    final messageSendEndpoint =
+        Uri.parse('https://graph.facebook.com/v20.0/390332304171825/messages');
     // final mansiEndpoint =
     //     Uri.parse('https://whxmgbtb-8000.inc1.devtunnels.ms/query');
 
@@ -89,64 +89,61 @@ Future<bool> replyToUser(String name, String messageBody,
     // const bearer =
     //     'EAAHoI1o82mEBOw0ZAAdAWmmO0sAhSmfKUKtyF9iXuE5yfbxPMUxPtprwZBv1AanJQx43WcxsKbIVMo3ekONoDTsujt8VwpkEDkvCgXXUetTEVgjelDsGRnTUqYfUDgay5hZBRbhM1oCnZC3hsSsnplZCAOuelMpR4dbdk7mHMkKUWCFCpPKvEodwBACqYGCeIuvD3viBoSOllMZAgJ2XLneHjQAvEtMrduyAlHXA8ZD';
     // logger.debug('\n\nSending message to user..\n\n');
-    // return Future.sync(() {
-    //   try {
-    // http.post(
-    //   mansiEndpoint,
-    //   body: jsonEncode({
-    //     'query': messageBody,
-    //     'uid': 'RQ2pIEzjVsb3zbKkckms7a3iOnC3',
-    //     'name': name,
-    //     'goal_weight': 70,
-    //     'current_weight': 75,
-    //     'bmi': 24,
-    //     'age': 22,
-    //     'gender': 'male',
-    //     'docid': 'fkTjWRTlT6OmiGihOs9y',
-    //   }),
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    // ).then((resp) {
-    //   if (resp.statusCode != 200) {
-    //     logger.error('Something went wrong: return ${resp.statusCode}');
-    //     return;
-    //   }
-    //   logger.debug('\n\nMansi Response is: ${resp.body}\n\n');
-    //     return http
-    //         .post(
-    //       messageSendEndpoint,
-    //       headers: {
-    //         'Authorization':
-    //             'Bearer EAAHoI1o82mEBO8arYFjaeoyQocDnWJ7myS20ZCSxdmFYDyd63KPW1aFqdMbEEcUgH8cdoUJpe67oH0ZCbuYfihZA7E1ZBfgRQo5JLBacDUoMLabegQbliOZCCTrCXKS2jBqlk99b4ahlZBtgBs7iQx04TkZBfZBkmAmWbpmnIZCZCrE98OiEv5RrZAHfIjcm7yXRxF8ZBZC2TpaSaOoy7XGBE0umU75i9MWZCGM6ZB90g8ZD',
-    //         'Content-Type': 'application/json'
-    //       },
-    //       body: jsonEncode({
-    //         'messaging_product': 'whatsapp',
-    //         'recipient_type': 'individual',
-    //         'to': '+916200052309',
-    //         'type': 'text',
-    //         'text': {
-    //           'preview_url': false,
-    //           'body':
-    //               // jsonDecode(resp.body)['response'] ?? 'No Response from Mansi',
-    //               'responding back..',
-    //         },
-    //       }),
-    //     )
-    //         .then((resp) {
-    //       logger.info(
-    //           'Response after sending message: ${resp.statusCode}\n\n${resp.body}\n\n');
-    //       return true;
-    //     });
-    //   } catch (er) {
-    //     logger.info('\nError in exec future $er\n');
-    //     return false;
-    //   }
-    // });
-    // });
-
-    // });
+    return Future.sync(() {
+      try {
+        // http.post(
+        //   mansiEndpoint,
+        //   body: jsonEncode({
+        //     'query': messageBody,
+        //     'uid': 'RQ2pIEzjVsb3zbKkckms7a3iOnC3',
+        //     'name': name,
+        //     'goal_weight': 70,
+        //     'current_weight': 75,
+        //     'bmi': 24,
+        //     'age': 22,
+        //     'gender': 'male',
+        //     'docid': 'fkTjWRTlT6OmiGihOs9y',
+        //   }),
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //   },
+        // ).then((resp) {
+        // if (resp.statusCode != 200) {
+        //   logger.error('Something went wrong: return ${resp.statusCode}');
+        //   return;
+        // }
+        // logger.debug('\n\nMansi Response is: ${resp.body}\n\n');
+        return http
+            .post(
+          messageSendEndpoint,
+          headers: {
+            'Authorization':
+                'Bearer EAAHoI1o82mEBO8arYFjaeoyQocDnWJ7myS20ZCSxdmFYDyd63KPW1aFqdMbEEcUgH8cdoUJpe67oH0ZCbuYfihZA7E1ZBfgRQo5JLBacDUoMLabegQbliOZCCTrCXKS2jBqlk99b4ahlZBtgBs7iQx04TkZBfZBkmAmWbpmnIZCZCrE98OiEv5RrZAHfIjcm7yXRxF8ZBZC2TpaSaOoy7XGBE0umU75i9MWZCGM6ZB90g8ZD',
+            'Content-Type': 'application/json'
+          },
+          body: jsonEncode({
+            'messaging_product': 'whatsapp',
+            'recipient_type': 'individual',
+            'to': '+916200052309',
+            'type': 'text',
+            'text': {
+              'preview_url': false,
+              'body':
+                  // jsonDecode(resp.body)['response'] ?? 'No Response from Mansi',
+                  'responding back..',
+            },
+          }),
+        )
+            .then((resp) {
+          logger.info(
+              'Response after sending message: ${resp.statusCode}\n\n${resp.body}\n\n');
+          return true;
+        });
+      } catch (er) {
+        logger.info('\nError in exec future $er\n');
+        return false;
+      }
+    });
   } catch (er) {
     logger.error('\n\nSomething Went Wrong: $er\n\n');
     return Future(() => false);
